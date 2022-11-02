c53459d697cb4c2d08eae21709cca11c18d29931
MMPN77 <117305360+MMPN77@users.noreply.github.com>
2022-11-03 01:36:26 +0330

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class boolean
+{
+    public static void Main()
+    {
+        bool content = true;
+        bool nocontent = false;
+        Console.WriteLine("Test is {0}", content);
+        Console.WriteLine("Test is {0}", nocontent);
+    }
+}
